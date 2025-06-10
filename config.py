7abdde1df092ec25e1e2c51fb53914f184b54d9a
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-06-10 16:37:24 -0300

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ import os
 load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
-TENANT_ID = os.getenv("TENANT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+TENANT_ID = os.getenv("TENANT_ID")
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL") 
