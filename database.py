451ad676904e19a1df6a819ea828907346f2b587
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-06-10 17:44:07 -0300

    Update database.py

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ fila = [
 # Index atual da fila (rotaciona a cada chamada)
 index_atual = 0
 
-async def get_proximo_vendedor():
+def get_proximo_vendedor():
     global index_atual
     vendedor = fila[index_atual]
     index_atual = (index_atual + 1) % len(fila)
