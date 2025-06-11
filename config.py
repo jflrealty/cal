1bacfbba9b9e9afd98b1407b0f9935696961fc13
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-06-11 17:26:22 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,14 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 TENANT_ID = os.getenv("TENANT_ID")
-ADMIN_EMAIL = os.getenv("ADMIN_EMAIL") 
+ADMIN_EMAIL = os.getenv("ADMIN_EMAIL")
+
 TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_WHATSAPP_NUMBER = os.getenv("TWILIO_WHATSAPP_NUMBER")
+TWILIO_MESSAGING_SERVICE_SID = os.getenv("TWILIO_MESSAGING_SERVICE_SID")
